c5f5bb0d6fdc561faff5a80c28277db25a377af1
Christopher Ochsenreither <ochsenrc@nala.roche.com>
2019-09-18 16:10:50 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# AD-LDAP-HELPER
+---
+
+A module with some helper functions to authenticate a Microsoft Active Directory user or lookup a user's info with a default account.
+
+Dependencies: 
+- dotenv
+- activedirectory2
+
+Requires .env file in root of project with the following environmental variables:
+- LDAP_URL: Active Directory server to connect to, e.g. ldap://ad.example.com
+- BASE_DN: The root DN from which all searches will be performed, e.g. dc=example,dc=com
+- AD_USER: default ad user (hopefully not a real user's account!)
+- AD_PASSWD: default ad user's password
+
+### Methods
+loginADUser(username: string, password: string, attributes?: Array<string>): User|null
+
+Attempts to authenticate a user with a given sAMAccountName and password. If unsuccessful, it returns null. If successful it returns a user object with:
+- sAMAccountName
+- givenName (firstname)
+- sn (surname)
+- telephoneNumber
+- mail (email)
+- l (location)
+- ... any attributes included in the optional 3rd argument (an array of strings)
+
+getADUserInfo(username: string, attributes?: Array<string>): Promise<User|null>
+Authenticates with a default user whose sAMAccountName and password are defined in a .env file. If unsuccessful, it returns a Promise rejection with null. If successful it returns a Promise resolve with the user object:
+- sAMAccountName
+- givenName (firstname)
+- sn (surname)
+- telephoneNumber
+- mail (email)
+- l (location)
+- ... any attributes included in the optional 3rd argument (an array of strings)
---
+++ index.js
@@ -0,0 +1,107 @@
+const dotenv = require('dotenv');
+const ActiveDirectory = require('activedirectory2');
+const getConfig = (username, password, extraAttributes) => {
+    dotenv.config();
+
+    attributes = [
+        'sAMAccountName', 
+        'givenName', 
+        'sn', 
+        'telephoneNumber', 
+        'mail',
+        'l',
+    ];
+
+    if (extraAttributes) {
+        attributes = [...attributes, ...extraAttributes];
+    }
+
+    return {
+        url: process.env.LDAP_URL,
+        baseDN: process.env.BASE_DN,
+        username: username || process.env.AD_USER,
+        password: password || process.env.AD_PASSWD,
+        attributes: {
+            user: attributes,
+        }
+    }
+};
+
+const getADUserInfo = (username) => {
+    const config = getConfig();
+    const ad = new ActiveDirectory(config);
+    
+    return new Promise((resolve, reject) => {
+        ad.authenticate(config.username, config.password, function(err, auth) {
+            if (err) {
+                console.log('ERROR: '+JSON.stringify(err));
+                reject(null);
+            }
+           
+            if (auth) {
+                console.log('Client user authenticated!');
+                console.log(`Finding user ${username}`);
+    
+                ad.findUser(username, function (err, user) {
+                    if (err) {
+                        console.log('ERROR: ' + JSON.stringify(err));
+                        reject(null);
+                    }
+        
+                    if (!user) {
+                        console.log(`User: ${username} not found.`);
+                        reject(null);   
+                    } else {
+                        console.log('Found user: ' + user.sAMAccountName);
+                        resolve(user);
+                    }
+                });
+            } else {
+                console.log('Authentication failed!');
+                reject(null);
+            }
+        });
+    });
+}
+
+const loginADUser = (username, password, attributes) => {
+    return new Promise((resolve, reject) => {
+        const config = getConfig(username, password, attributes);   
+        const ad = new ActiveDirectory(config);
+        
+        ad.authenticate(config.username, password, function(err, auth) {
+            if (err) {
+                console.log('ERROR: '+JSON.stringify(err));
+                reject(null);
+            }
+           
+            if (auth) {
+                console.log('Client user authenticated!');
+                console.log(`Finding user ${username}`);
+    
+                ad.findUser(username, function (err, user) {
+                    if (err) {
+                        console.log('ERROR: ' + JSON.stringify(err));
+                        reject(null);
+                    }
+        
+                    if (!user) {
+                        console.log(`User: ${username} not found.`);
+                        reject(null);   
+                    } else {
+                        console.log('Found user: ' + user.sAMAccountName);
+                        resolve(user);
+                    }
+                });
+            } else {
+                console.log('Authentication failed!');
+                reject(null);
+            }
+        });
+    });
+}
+
+module.exports = {
+    getADUserInfo,
+    loginADUser,
+}
