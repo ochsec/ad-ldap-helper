52914bf826905849d5d49e47d7c8261f2e507191
Christopher Ochsenreither <ochsenrc@nala.roche.com>
2019-10-02 13:18:56 -0700

    send back err msg string

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const dotenv = require('dotenv');
 const ActiveDirectory = require('activedirectory2');
+
+const errors = {
+    AUTH_FAILED: 'Authentication failed!',
+    NOT_FOUND: (username) => `User: ${username} not found.`,
+    GENERIC: (error) => `Error: ${JSON.stringify(error)}`,
+};
+
 const getConfig = (username, password, extraAttributes) => {
     dotenv.config();
 
@@ -34,8 +41,8 @@ const getADUserInfo = (username, attributes) => {
     return new Promise((resolve, reject) => {
         ad.authenticate(config.username, config.password, function(err, auth) {
             if (err) {
-                console.log('ERROR: '+JSON.stringify(err));
-                reject(null);
+                console.log(errors.GENERIC(err));
+                reject(errors.GENERIC(err));
             }
            
             if (auth) {
@@ -44,21 +51,22 @@ const getADUserInfo = (username, attributes) => {
     
                 ad.findUser(username, function (err, user) {
                     if (err) {
-                        console.log('ERROR: ' + JSON.stringify(err));
-                        reject(null);
+                        console.log(errors.GENERIC(err));
+                        reject(errors.GENERIC(err));
                     }
         
                     if (!user) {
-                        console.log(`User: ${username} not found.`);
-                        reject(null);   
+                        console.log(errors.NOT_FOUND(username));
+                        reject(errors.NOT_FOUND(username));   
                     } else {
-                        console.log('Found user: ' + user.sAMAccountName);
+                        const msg = 'Found user: ' + user.sAMAccountName
+                        console.log(msg);
                         resolve(user);
                     }
                 });
             } else {
-                console.log('Authentication failed!');
-                reject(null);
+                console.log(errors.AUTH_FAILED);
+                reject(errors.AUTH_FAILED);
             }
         });
     });
@@ -71,8 +79,8 @@ const loginADUser = (username, password, attributes) => {
         
         ad.authenticate(config.username, password, function(err, auth) {
             if (err) {
-                console.log('ERROR: '+JSON.stringify(err));
-                reject(null);
+                console.log(errors.GENERIC(err));
+                reject(errors.GENERIC(err));
             }
            
             if (auth) {
@@ -81,21 +89,22 @@ const loginADUser = (username, password, attributes) => {
     
                 ad.findUser(username, function (err, user) {
                     if (err) {
-                        console.log('ERROR: ' + JSON.stringify(err));
-                        reject(null);
+                        const msg = 'ERROR: ' + JSON.stringify(err);
+                        console.log(msg);
+                        reject(msg);
                     }
         
                     if (!user) {
-                        console.log(`User: ${username} not found.`);
-                        reject(null);   
+                        console.log(errors.NOT_FOUND(username));
+                        reject(errors.NOT_FOUND(username));   
                     } else {
                         console.log('Found user: ' + user.sAMAccountName);
                         resolve(user);
                     }
                 });
             } else {
-                console.log('Authentication failed!');
-                reject(null);
+                console.log(errors.AUTH_FAILED);
+                reject(errors.AUTH_FAILED);
             }
         });
     });
